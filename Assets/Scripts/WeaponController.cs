cc8bec41dca9f3b79522f280840e30c77d0fea0e
ZeroJustMe <619378845@qq.com>
2017-12-27 00:36:27 +0800

    增加施法间隔
    可在预制体中脚本的公有变量调节频率

--- Assets/Scripts/WeaponController.cs
+++ Assets/Scripts/WeaponController.cs
@@ -12,23 +12,30 @@ public class WeaponController : MonoBehaviour {
 	void Start () {
         Player = GameObject.Find("Player/Animation").GetComponent<Animator>();
         Weapon = GameObject.Find("Player/weapon");
+        lastFiretime = Time.time;
+
     }
 	
 	// Update is called once per frame
 	void Update () {
         Vector3 wpPosition = transform.position;
         if (Input.GetKey("j")) {
-        //    if(Time.time>lastFiretime)
-            Player.SetBool("PA", true);
-            Player.SetBool("Boom", false);
-            GameObject pa = Instantiate(Resources.Load("Prefebs/PA") as GameObject) as GameObject;
-            pa.transform.localPosition = wpPosition;
+            if (Time.time > lastFiretime + 1 / frequencyPA) {
+                Player.SetBool("PA", true);
+                Player.SetBool("Boom", false);
+                GameObject pa = Instantiate(Resources.Load("Prefebs/PA") as GameObject) as GameObject;
+                pa.transform.localPosition = wpPosition;
+                lastFiretime = Time.time;
+            }
         }
         else if (Input.GetKey("h")) {
-            Player.SetBool("Boom", true);
-            Player.SetBool("PA", false);
-            GameObject boom = Instantiate(Resources.Load("Prefebs/Boom") as GameObject) as GameObject;
-            boom.transform.localPosition = wpPosition;
+            if (Time.time > lastFiretime + 1 / frequencyBoom) {
+                Player.SetBool("Boom", true);
+                Player.SetBool("PA", false);
+                GameObject boom = Instantiate(Resources.Load("Prefebs/Boom") as GameObject) as GameObject;
+                boom.transform.localPosition = wpPosition;
+                lastFiretime = Time.time;
+            }
         }
         else {
             Player.SetBool("PA", false);
