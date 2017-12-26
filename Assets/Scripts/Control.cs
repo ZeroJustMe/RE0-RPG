3f424a492a64d3c1b6c7dcb426d2389667c18fa5
ROROBINBIN <464214931@qq.com>
2017-12-26 17:13:33 +0800

    Rewrite

---
+++ Assets/Scripts/Control.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour {
+
+    float HorizontalSpeed, VerticalSpeed;
+
+    Animator Player;
+
+    // Use this for initialization
+    void Start () {
+        HorizontalSpeed  = (float)3.8;
+        VerticalSpeed    = (float)3.0;
+
+        Player = GameObject.Find("Player/Animation").GetComponent<Animator>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+        float H = Input.GetAxis("Horizontal");
+        float V = Input.GetAxis("Vertical");
+        if (H == 0 && V == 0) { Player.SetBool("IfRun", false); return; }
+
+        Player.SetBool("IfRun", true);
+        transform.position += new Vector3(H * HorizontalSpeed, V * VerticalSpeed, V * VerticalSpeed) * Time.deltaTime;
+
+        if (H > 0)transform.rotation = Quaternion.Euler(0, 0, 0);
+        else if(H < 0)transform.rotation = Quaternion.Euler(0, 180f, 0);
+    }
+}
---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour {
+
+    bool HaveEntered = false;
+
+    float StartTime, NowTime;
+
+    Vector3 PreCamera, PrePlayer;
+
+    GameObject Enemy = null, Camera;
+    Collider2D Player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        NowTime = Time.time;
+        if (Enemy != null && NowTime - StartTime >= 5)
+        {
+            Camera.transform.position = PreCamera;
+            Player.transform.position = PrePlayer;
+            Destroy(Enemy);
+            HaveEntered = true;
+            Enemy = null;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (HaveEntered) { HaveEntered = false; return;}
+        StartTime = Time.time;
+
+        Enemy = Instantiate(Resources.Load("Prefebs/Enemy") as GameObject) as GameObject;
+        Enemy.transform.localScale = new Vector3((float)0.0073, (float)0.0073, 1);
+        Enemy.transform.localPosition = new Vector3((float)5.4, (float)-10, -10);
+
+        Player = other;
+        PrePlayer = Player.transform.position;
+        Player.transform.localPosition = new Vector3((float)-7, (float)-10, -10);
+
+        Camera = GameObject.Find("Main Camera");
+        PreCamera = Camera.transform.position;
+        Camera.transform.localPosition = new Vector3((float)0, (float)-10, -100);
+    }
+}
